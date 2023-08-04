9e43240095c498019a434a09b169821ad694fdcb
UdayYaragalla <yaragallauday@gmail.com>
2023-08-04 09:50:15 +0530

    added header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
-  );
-}
+    <Header />
+      )}
 
 export default App;
